f169eaa4b276cbdcf613a656ae02ddb27a869204
Remy Meier <remad@web.de>
2024-09-03 20:20:15 +0200

    master: MenuComponent was bootstrapped as Element from AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MenuComponent } from "./menu/menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
